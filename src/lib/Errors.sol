0b2c33e396a968b741adc2a8c54c4856d8211a42
neeel.eth <NelsonGaldeman@users.noreply.github.com>
2025-06-18 21:25:04 -0400

    propa errors and back to ownable (#11)
    * propa errors and back to ownable
    
    * fix: review
    
    * vm.store true whitelist to bypass whitelist in tests
    
    ---------
    
    Co-authored-by: lukmaan-k <lukmaankolia@gmail.com>

--- src/lib/Errors.sol
+++ src/lib/Errors.sol
@@ -1,42 +1,21 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.30;
+pragma solidity 0.8.30;
 
 library Errors {
-  string constant INVALID_ORACLE_PRICE = "1";
-  string constant CYCLE_ALREADY_STARTED = "2";
-  string constant PREVIOUS_CYCLE_NOT_SETTLED = "3";
-  string constant INVALID_AMOUNT = "4";
-  string constant INSUFFICIENT_BALANCE = "5";
-  string constant NOT_OWNER = "6";
-  string constant NOT_WHITELISTED = "7";
-  string constant INVALID_SIGNATURE = "8";
-  string constant ORACLE_PRICE_CALL_FAILED = "9";
-  string constant CYCLE_ACTIVE = "10";
-  string constant STILL_SOLVENT = "11";
-  string constant IN_TRADER_LIST = "12";
-  string constant NOT_EXPIRED = "13";
-  string constant PRICE_NOT_FIXED = "14";
-  string constant CYCLE_ALREADY_SETTLED = "15";
-  string constant MARKET_NOT_LIVE = "16";
-  string constant TICK_TOO_LARGE = "17";
-
-  error InvalidOraclePrice();
-  error CycleAlreadyStarted();
-  error PreviousCycleNotSettled();
   error InvalidAmount();
   error InsufficientBalance();
-  error NotOwner();
+  error NotOrderOwner();
   error NotWhitelisted();
-  error InvalidSignature();
+  error InvalidWhitelistSignature();
   error OraclePriceCallFailed();
   error CycleActive();
   error StillSolvent();
   error InTraderList();
   error NotExpired();
-  error PriceNotFixed();
   error CycleAlreadySettled();
   error MarketNotLive();
   error TickTooLarge();
-  error MaximumOrdersCapReached();
   error AccountInLiquidation();
+  error TraderLiquidatable();
+  error NotSecurityCouncil();
 }
